30c4fc5abfac5f084ab65efdba8e2aa0129daa2f
Raman Kumar <jobs.er.raman@gmail.com>
2023-09-16 00:48:59 +0530

    completed

--- src/components/ShipCard/ShipCard.js
+++ src/components/ShipCard/ShipCard.js
@@ -1,75 +1,124 @@
-import React,{useEffect, useState} from 'react';
-import styles from './ShipCard.module.scss';
+// import React,{useEffect, useState} from 'react';
+// import styles from './ShipCard.module.scss';
 
+
+// function ShipCard({ ship, isMostFilms }) {
+//     const [shipName, setShipName] = useState(ship.model);
+//     const [image, setImages] = useState([]);
+//     const [loading, setLoading] = useState(false);
+//     const apiKey = 'a96c42f292f39f57f52996a71dc7c784'; // Replace with your Flickr API key
+//     useEffect(() => {
+//         const getImg = async () => {
+//             setLoading(true);
+          
+//             try {
+//               const response = await fetch(
+//                 `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${shipName}&format=json&nojsoncallback=1`
+//               );
+          
+//               if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//               }
+          
+//               const data = await response.json();
+          
+//               if (data.stat === 'ok') {
+//                 setImages(data.photos.photo[0]);
+//               } else {
+//                 console.error('Flickr API error:', data.message);
+//               }
+//             } catch (error) {
+//               console.error('Error fetching data:', error);
+//             } finally {
+//               setLoading(false);
+//             }
+//           };
+
+//           getImg();
+//     },[]);
+//     return (
+//         <>
+//         <div className={styles.card}>
+//             <div className={styles.cardImage}> <img
+//             width="250"
+//             key={image.id}
+//             src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
+//             alt={image.title}
+//         /></div>
+//          <div>
+//             <div className={styles.category}>Ship Name :- {ship.name} </div>
+//             <div className={styles.heading}> Model: {ship.model}
+//                 <div className={styles.author}> Number of Films: {ship.films.length} <span className={styles.name}>Number of crew: {ship.crew}</span> </div>
+//             </div>   
+//             </div>
+//             <div>{isMostFilms && <img width="50" src="https://cdn4.iconfinder.com/data/icons/business-management-223/24/idea_innovation_management_change_company_business_corporate-1024.png" alt="Most Films Icon" />}</div>
+//         </div>
+        
+//         </>
+//     );
+// }
+
+// export default ShipCard;
+
+
+
+import React, { useEffect, useState } from 'react';
+import styles from './ShipCard.module.scss';
+import { fetchShipImage } from "../../services/api"// Import the fetchShipImage function from api.js
 
 function ShipCard({ ship, isMostFilms }) {
-    const [shipName, setShipName] = useState(ship.model);
-    const [image, setImages] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const apiKey = 'a96c42f292f39f57f52996a71dc7c784'; // Replace with your Flickr API key
+    const [image, setImage] = useState(null); // Initialize image state with null
+
     useEffect(() => {
         const getImg = async () => {
-            setLoading(true);
-          
             try {
-              const response = await fetch(
-                `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${shipName}&format=json&nojsoncallback=1`
-              );
-          
-              if (!response.ok) {
-                throw new Error('Network response was not ok');
-              }
-          
-              const data = await response.json();
-          
-              if (data.stat === 'ok') {
-                setImages(data.photos.photo[0]);
-              } else {
-                console.error('Flickr API error:', data.message);
-              }
+                const shipImage = await fetchShipImage(ship.model); // Call the fetchShipImage function with ship.model
+                if (shipImage) {
+                    setImage(shipImage); // Set the image state with the fetched image data
+                }
             } catch (error) {
-              console.error('Error fetching data:', error);
-            } finally {
-              setLoading(false);
+                console.error('Error fetching image:', error);
             }
-          };
+        };
 
-          getImg();
-    },[]);
+        getImg();
+    }, [ship.model]); // Update image when ship.model changes
+
     return (
         <>
-        <div className={styles.card}>
-            <div className={styles.cardImage}> <img
-            width="250"
-            key={image.id}
-            src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
-            alt={image.title}
-        /></div>
-         <div>
-            <div className={styles.category}>Ship Name :- {ship.name} </div>
-            <div className={styles.heading}> Model: {ship.model}
-                <div className={styles.author}> Number of Films: {ship.films.length} <span className={styles.name}>Number of crew: {ship.crew}</span> </div>
-            </div>   
+            <div className={styles.card}>
+                <div className={styles.cardImage}>
+                    {image && (
+                        <img
+                            width="250"
+                            key={image.id}
+                            src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
+                            alt={image.title}
+                        />
+                    )}
+                </div>
+                <div>
+                    <div className={styles.category}>Ship Name :- {ship.name}</div>
+                    <div className={styles.heading}>
+                        Model: {ship.model}
+                        <div className={styles.author}>
+                            Number of Films: {ship.films.length}{' '}
+                            <span className={styles.name}>Number of crew: {ship.crew}</span>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    {isMostFilms && (
+                        <img
+                            width="50"
+                            src="https://cdn4.iconfinder.com/data/icons/business-management-223/24/idea_innovation_management_change_company_business_corporate-1024.png"
+                            alt="Most Films Icon"
+                        />
+                    )}
+                </div>
             </div>
-            <div>{isMostFilms && <img width="50" src="https://cdn4.iconfinder.com/data/icons/business-management-223/24/idea_innovation_management_change_company_business_corporate-1024.png" alt="Most Films Icon" />}</div>
-        </div>
-        {/* <div className={styles.card}>
-        <img
-            key={image.id}
-            src={`https://farm${image.farm}.staticflickr.com/${image.server}/${image.id}_${image.secret}.jpg`}
-            alt={image.title}
-        />
-            <h2>{ship.name}</h2>
-            <p>Model: {ship.model}</p>
-            <p>Number of Films: {ship.films.length}</p>
-            <p>Number of crew: {ship.crew}</p>
-            {isMostFilms && <img width="50" src="https://cdn4.iconfinder.com/data/icons/business-management-223/24/idea_innovation_management_change_company_business_corporate-1024.png" alt="Most Films Icon" />}
-        </div> */}
         </>
     );
 }
 
 export default ShipCard;
-
-
-
--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,27 @@
+// const SWAPI_BASE_URL = 'https://swapi.dev/api';
+
+// async function fetchStarships() {
+//     try {
+//         const response = await fetch(`${SWAPI_BASE_URL}/starships`);
+//         if (!response.ok) {
+//             throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         // Sort and filter the data as required
+//         const sortedAndFilteredStarships = data.results
+//             .filter((starship) => parseInt(starship.crew) <= 10)
+//             .sort((a, b) => parseInt(a.crew) - parseInt(b.crew));
+//         console.log(sortedAndFilteredStarships);
+//         return sortedAndFilteredStarships;
+//     } catch (error) {
+//         console.error('Error fetching or processing starships:', error);
+//         throw error;
+//     }
+// }
+
+// export { fetchStarships };
+
+
 const SWAPI_BASE_URL = 'https://swapi.dev/api';
 
 async function fetchStarships() {
@@ -19,9 +43,32 @@ async function fetchStarships() {
     }
 }
 
-export { fetchStarships };
+async function fetchShipImage(shipName) {
+    const apiKey = 'a96c42f292f39f57f52996a71dc7c784'; // Replace with your Flickr API key
+    try {
+        const response = await fetch(
+            `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=${shipName}&format=json&nojsoncallback=1`
+        );
 
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
 
+        const data = await response.json();
+
+        if (data.stat === 'ok') {
+            return data.photos.photo[0];
+        } else {
+            console.error('Flickr API error:', data.message);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+
+export { fetchStarships, fetchShipImage };
 
 
 
